0019907a2355438ea0238e037705c3f0b04d9dd6
AdityaSutanto <aditya.422024013@civitas.ukrida.ac.id>
2025-09-18 16:24:18 +0700

    My First Commit

---
+++ app/Models/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function get_product(){
+        $sql = $this->select("products.*", "category_product.product_category_name as product_category_name1", "supplier.supplier_name as supplier_name1")
+                    
+                    ->join('category_product', 'category_product.id', '=', 'products.product_category_id')
+                    ->join('supplier', 'supplier.id', '=', 'products.supplier_id');
+        return $sql;
+    }
+};
